8af514d8137dae93552f53ebeeccfc3cf3df46b7
aman3002 <amangarg3002@gmail.com>
2024-07-05 23:52:42 +0530

    server

--- backend/passport.js
+++ backend/passport.js
@@ -5,7 +5,7 @@ require("dotenv").config()
 passport.use(new GoogleStrategy({
     clientID: "492147582846-cnvmg9au6p1e92mjelabvk7h2io7coii.apps.googleusercontent.com",
     clientSecret: "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",
-    callbackURL: `${process.env.REDIRECT}/auth/google/callback`,
+    callbackURL: `${process.env.REACT}/auth/google/callback`,
     passReqToCallback: true,
     scope: ['profile', 'email']
 }, function (req, accessToken, refreshToken, profile, done) {
